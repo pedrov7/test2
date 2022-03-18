6c8eb37b2388c74961f58816050e793d9bc2b6dd
Jorge Cajamarca <jorge.cajamarca@AME.LOCAL>
2022-03-18 16:57:58 -0500

    new

--- client/src/view/Main.js
+++ client/src/view/Main.js
@@ -4,13 +4,33 @@ import { LoginView } from './LoginView'
 
 import firebaseApp from '../credentials'
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
+import { getFirestore, doc, getDoc} from 'firebase/firestore'
+
 const auth = getAuth(firebaseApp)
+const firestore = getFirestore(firebaseApp)
 
 export const Main = () => {
     const [userAuth, setUserAuth] = useState(null)
-
+    const [user, setUser] = useState(null)
+    async function getRole(uid){
+      const docuRef = doc(firestore, `/Usuarios/${uid}`);
+      const docuCifrada = await getDoc(docuRef);
+      const role = docuCifrada.data().role
+      return role
+    }
     onAuthStateChanged(auth, (userFirebase) => {
       if(userFirebase){
+        if(!user){
+          getRole(userFirebase.uid).then(role =>{
+            const userData = {
+              uid: userFirebase.uid,
+              emai: userFirebase.email,
+              role: role
+            }
+            setUser(userData);
+            console.log(userData)
+          })
+        }
         setUserAuth(userFirebase)
       }else{
         setUserAuth(null)
